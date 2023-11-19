6247e4d191deb84d3dea8d681640545fd53bf3dd
gona79 <cg.vera.07@gmail.com>
2023-11-18 22:47:44 -0300

    Se modifica y actualiza estructura y readme segun solicitado en 2da preentrega

--- src/components/itemlistcontainer/ItemListContainer.jsx
+++ src/components/itemlistcontainer/ItemListContainer.jsx
@@ -1,9 +1,26 @@
 import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import ItemList from '../itemlist/ItemList'; // Asegúrate de que la ruta sea correcta
 
 const ItemListContainer = ({ greeting }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getItems = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([{ id: 1, title: "Producto 1", price: 100 }, { id: 2, title: "Producto 2", price: 200 }]); // Simula datos de productos
+      }, 2000);
+    });
+
+    getItems.then((data) => {
+      setItems(data);
+    });
+  }, []);
+
   return (
     <div className="container mt-4">
       <h2>{greeting}</h2>
+      <ItemList items={items} /> {/* Muestra los productos */}
     </div>
   );
 };
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,28 +1,25 @@
 import { Navbar, Nav, Badge } from 'react-bootstrap';
 import './NavBar.css'; // Importa los estilos
+import CartWidget from '../cartwidget/CartWidget'; // Importa el CartWidget
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg" className="nav-centered">
-      <Navbar.Brand href="#home">Alfa y Omega Estética 🌸</Navbar.Brand>
+      <Navbar.Brand href="/">Alfa y Omega Estética 🌸</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="menu-center">
-          <Nav.Link href="#depilacion">Depilación</Nav.Link>
-          <Nav.Link href="#masajes">Masajes</Nav.Link>
-          <Nav.Link href="#productos">Productos</Nav.Link>
-          <Nav.Link href="#cursos">Cursos</Nav.Link>
-          <Nav.Link href="#contacto">Contacto</Nav.Link>
+          <Nav.Link href="/category/depilacion">Depilación</Nav.Link>
+          <Nav.Link href="/category/masajes">Masajes</Nav.Link>
+          <Nav.Link href="/category/productos">Productos</Nav.Link>
+          <Nav.Link href="/category/cursos">Cursos</Nav.Link>
+          <Nav.Link href="/contacto">Contacto</Nav.Link>
         </Nav>
       </Navbar.Collapse>
       <Nav>
-        <Nav.Link href="#carrito">
-          <i className="fa fa-shopping-cart"></i>
-          <Badge pill variant="info">3</Badge>
-        </Nav.Link>
+        <CartWidget />
       </Nav>
     </Navbar>
   );
 }
-
 export default NavBar;
